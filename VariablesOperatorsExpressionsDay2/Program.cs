28507e4382f5f01931485897103ebd43fa77b5f3
HollyNLittle <hollyisalwayshopeful@gmail.com>
2017-08-15 20:50:38 -0400

    Created more variable types

--- VariablesOperatorsExpressionsDay2/Program.cs
+++ VariablesOperatorsExpressionsDay2/Program.cs
@@ -10,6 +10,30 @@ namespace VariablesOperatorsExpressionsDay2
     {
         static void Main(string[] args)
         {
+            string name = "Holly";
+            //Console.WriteLine(name);
+
+            name = "Dave";
+            //Console.WriteLine(name);
+     
+            int birthMonthNumber = 3;
+            int age = 44;
+            int population = 1000000;
+            //floating type variables
+            float number = 2.164403304897294383961032f;
+            double someNumber = 2.164403304897294383961032;
+            decimal partialNumber = 2.164403304897294383961032m;
+
+            int amountPaidEach = 13;
+            int numberOfFriends = 4;
+            int totalBill = amountPaidEach * numberOfFriends;
+            Console.WriteLine(totalBill);
+
+
+         
+ 
+
+
         }
     }
 }
