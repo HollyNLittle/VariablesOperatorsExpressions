1ff8d526ca6eb79aa37b9d97c48801bab83fb5e9
HollyNLittle <hollyisalwayshopeful@gmail.com>
2017-08-16 20:34:41 -0400

    practice problems slide 11 Wk1Day3

--- VariablesOperatorsExpressionsDay2/Program.cs
+++ VariablesOperatorsExpressionsDay2/Program.cs
@@ -47,6 +47,75 @@ namespace VariablesOperatorsExpressionsDay2
             decimal norachaisMoneyBeforePurchase = norachaisTotalMoney + moneyPaidForSalad;
             Console.WriteLine("Amount of money Norachai had prior to purchase: {0}", norachaisMoneyBeforePurchase);
 
+            int studentsAttendedFieldTrip = 331;
+            int studentsInCars = 7;
+            int studentsTravellingBybus = studentsAttendedFieldTrip - studentsInCars;
+            int numOfBusses = 6;
+            int studentsPerBus = studentsTravellingBybus / numOfBusses;
+            Console.WriteLine("The number of students in each bus: {0}", studentsPerBus);
+
+            float pencilBudget = 24;
+            float numPencils = 7;
+            float moneyAfterPencilPurchase = 10;
+            float totalSpentOnPencils = pencilBudget - moneyAfterPencilPurchase;
+            float costPerPencil = totalSpentOnPencils / numPencils;
+            Console.WriteLine("The cost per pencil: {0}", costPerPencil);
+
+            int boxesBought = 7;
+            int boxesDestroyed = 2;
+            int boxesRemaining = 22;
+            int originalBoxTotal = boxesRemaining * boxesDestroyed;
+            int boxesAtBeginning = originalBoxTotal - boxesBought;
+            Console.WriteLine("The number of boxes Maria had before the fire: {0}",
+                originalBoxTotal);
+            Console.WriteLine("The number of boxes Maria had before purchasing the 7 additional pencils: {0}", 
+                boxesAtBeginning);
+
+            int number1 = 23;
+            int number2 = 24;
+            int number3 = 25;
+            int sumOfNumbers = 72;
+            int numOfNumbers = 3;
+            int numberAverage = sumOfNumbers / numOfNumbers;
+            int amountToDecrease = -1;
+            int numberA = numberAverage - 1;
+            int x = numberAverage;
+            int numberC = numberAverage + 1;
+            Console.WriteLine("The smallest number is: {0}",
+                numberA);
+
+            int sum = 48;
+            int numOfNums = 3;
+            int average = sum / numOfNums;
+            int largestNum = average + 2;
+            int smallestNum = average - 2;
+            Console.WriteLine("The smallest number is; {0}", smallestNum);
+
+            int total = 48;
+            int min1 = (total - 6)/3;
+            Console.WriteLine(min1);
+
+            int sumOfEvenNumbers = 48;
+            int evenOne = sumOfEvenNumbers / 3;
+            int evenTwo = evenOne - 2;
+            Console.WriteLine(evenTwo);
+
+            int totalNum = 72;
+            int middleNum = totalNum / 3;
+            int largeNum = middleNum + 1;
+            int smallNum = middleNum - 1;
+            Console.WriteLine("The smallest number: {0}", smallNum );
+
+            int boughtBoxes = 7;
+            int boxesLeft = 22;
+            int boxesBeforeFire = boxesLeft * 2;
+            Console.WriteLine(boxesBeforeFire);
+
+
+
+
+
+
 
 
 
